700ba38c0e0d0bdb836c849efc8ce2999bad2c49
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-12-28 19:56:18 -0500

    fix: certificate path

--- wallet.d.ts
+++ wallet.d.ts
@@ -1,14 +1,14 @@
 // wallet.d.ts
 declare module "chia-wallet" {
   interface Config {
-    full_node_host: string;
-    wallet_host: string;
-    certificate_folder_path: string;
-    default_wallet_id: number;
-    default_fee: number;
-    default_mirror_coin_amount: number;
-    allowUnverifiedCert: boolean;
-    verbose: boolean;
+    full_node_host?: string;
+    wallet_host?: string;
+    certificate_folder_path?: string;
+    default_wallet_id?: number;
+    default_fee?: number;
+    default_mirror_coin_amount?: number;
+    allowUnverifiedCert?: boolean;
+    verbose?: boolean;
   }
 
   interface PushTxRequest {
